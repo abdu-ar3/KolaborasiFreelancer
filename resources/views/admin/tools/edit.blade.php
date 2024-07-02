3e8e218058ee0c075989b5a181176a1a0fed2858
abdu-ar3 <abdhurohman7@gmail.com>
2024-07-02 10:55:37 +0700

    Fitur Tool Crud

--- resources/views/admin/tools/edit.blade.php
+++ resources/views/admin/tools/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('New Tool') }}
+            {{ __('Edit Tool') }}
         </h2>
     </x-slot>
 
@@ -10,32 +10,37 @@
             <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
 
                 @if($errors->any())
-                    @foreach($errors->all() as $error)
-                        <div class="py-3 w-full rounded-3xl bg-red-500 text-white">
-                            {{$error}}
-                        </div>
-                    @endforeach
+                @foreach($errors->all() as $error)
+                <div class="py-3 w-full rounded-3xl bg-red-500 text-white">
+                    {{$error}}
+                </div>
+                @endforeach
                 @endif
-                
-                <form method="POST" action="#" enctype="multipart/form-data">
+
+                <form method="POST" action="{{route('admin.tools.update', $tool)}}" enctype="multipart/form-data">
                     @csrf
+                    @method('put')
 
                     <div>
                         <x-input-label for="name" :value="__('Name')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                            value="{{$tool->name}}" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
                         <x-input-label for="icon" :value="__('icon')" />
-                        <x-text-input id="icon" class="block mt-1 w-full" type="file" name="icon" required autofocus autocomplete="icon" />
+                        <img src="{{Storage::url($tool->icon)}}" alt=""
+                            class="rounded-2xl object-cover w-[120px] h-[90px]">
+                        <x-text-input id="icon" class="block mt-1 w-full" type="file" name="icon" autofocus
+                            autocomplete="icon" />
                         <x-input-error :messages="$errors->get('icon')" class="mt-2" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
-            
+
                         <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
-                            Add New Tools
+                            Update Tool
                         </button>
                     </div>
                 </form>
