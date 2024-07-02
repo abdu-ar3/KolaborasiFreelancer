0452dbf9a348b0d7609db342864ebcee7927b84a
abdu-ar3 <abdhurohman7@gmail.com>
2024-07-02 10:19:26 +0700

    Update Category

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -10,32 +10,38 @@
             <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
 
                 @if($errors->any())
-                    @foreach($errors->all() as $error)
-                        <div class="py-3 w-full rounded-3xl bg-red-500 text-white">
-                            {{$error}}
-                        </div>
-                    @endforeach
+                @foreach($errors->all() as $error)
+                <div class="py-3 w-full rounded-3xl bg-red-500 text-white">
+                    {{$error}}
+                </div>
+                @endforeach
                 @endif
-                
-                <form method="POST" action="#" enctype="multipart/form-data">
+
+                <form method="POST" action="{{route('admin.categories.update', $category)}}"
+                    enctype="multipart/form-data">
                     @csrf
+                    @method('put')
 
                     <div>
                         <x-input-label for="name" :value="__('Name')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                            value="{{$category->name}}" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
                         <x-input-label for="icon" :value="__('icon')" />
-                        <x-text-input id="icon" class="block mt-1 w-full" type="file" name="icon" required autofocus autocomplete="icon" />
+                        <img src="{{Storage::url($category->icon)}}" alt=""
+                            class="rounded-2xl object-cover w-[120px] h-[90px]">
+                        <x-text-input id="icon" class="block mt-1 w-full" type="file" name="icon" autofocus
+                            autocomplete="icon" />
                         <x-input-error :messages="$errors->get('icon')" class="mt-2" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
-            
+
                         <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
-                            Add New Category
+                            Update Category
                         </button>
                     </div>
                 </form>
